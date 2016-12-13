31efef21925370dae2e78cce2edfbe29a6be800f
theikeofficial <ajzenhamernikola@gmail.com>
2016-12-13 20:26:41 +0100

    Fixed GUI

--- public/index.php
+++ public/index.php
@@ -10,10 +10,6 @@ require_once '../vendor/twig/twig/lib/Twig/Autoloader.php';
 Twig_Autoloader::register();
 
 $loader = new Twig_Loader_Filesystem('../templates/');
-$twig = new Twig_Environment($loader, array(
-    'cache' => '../templates/cache',
-));
+$twig = new Twig_Environment($loader, array());
 
-//$template = $twig->load('home.php');
-
-echo $twig->render('home.php');
+echo $twig->render('home.html');
