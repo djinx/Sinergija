bfa770b0fffecb4d5b9c994ab2675e4da94c56f5
djinx <key13043@gmail.com>
2016-12-28 19:29:07 +0100

    Added option for changing picture

--- templates/home.php
+++ templates/home.php
@@ -6,6 +6,12 @@
 
 <div class="row" id="main-row">
     <div class="medium-3 columns show-for-medium">
+        <img id="korisnik_slika" src=" {{ session['Slika'] }}">
+        <form  enctype="multipart/form-data"  method="post" action="../sql/upload.php" name="upload" id="upload">
+            <input type="file" name="nova_slika" required>
+            <input type="hidden" name="nadimak" value="{{ session['Nadimak'] }}">
+            <input type="submit" name="izmeni" value="Izmeni"><br>
+        </form>
         <table>
             <tr><td id="ime"> {{ session['Ime'] ~ ' ' ~ session['Prezime'] }} </td></tr>
             <tr><td id="telefon"> {{ session['Telefon'] }}  </td></tr>
