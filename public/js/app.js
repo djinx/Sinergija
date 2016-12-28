8eaffeeb47c03fbd4f7939a190957e3840b2c944
theikeofficial <ajzenhamernikola@gmail.com>
2016-12-28 19:01:53 +0100

    Added GUI for creating a new user

--- public/js/app.js
+++ public/js/app.js
@@ -34,3 +34,72 @@ $(document).ready(function(){
     });*/
 });
 
+$("#kreirajClana").on("click", function() {
+    var formular =
+        "<div id='formular-noviClan'>" +
+        "       <div class='row column'>" +
+        "           <form data-abide novalidate>" +
+        "           <div data-abide-error class='alert callout' style='display: none;'>" +
+        "               <p><i class='fi-alert'></i> Postoje greške u formularu.</p>" +
+        "           </div>" +
+        "               <div class='small-12 medium-6 columns'>" +
+        "                   <label>Ime" +
+        "                   <input type='text' id='ImeClana' name='ImeClana' required></label>" +
+        "                   <label>Prezime" +
+        "                   <input type='text' id='PrezimeClana' name='PrezimeClana' required></label>" +
+        "                   <label>Broj telefona" +
+        "                   <input type='text' id='BrojTelefonaClana' name='BrojTelefonaClana' required</label>" +
+        "                   <label>Nadimak" +
+        "                   <input type='text' id='NadimakClana' name='NadimakClana' required</label>" +
+        "               </div>" +
+        "               <div class='small-12 medium-6 columns'>" +
+
+        "                   <label>E-mail" +
+        "                   <input type='email' id='EmailClana' name='EmailClana' required></label>" +
+        "                   <label>Lozinka" +
+        "                   <input type='password' id='password' name='password' required></label>" +
+        "                   <label>Ponovi lozinku" +
+        "                   <input type='password' required data-equalto='password'></label>" +
+        "                   <span class='form-error'>" +
+        "                       Lozinke moraju biti jednake!" +
+        "                   </span>" +
+        "                   <label>Odaberite tip korisnika" +
+        "                       <select id='select' required>" +
+        "                           <option value=''></option>" +
+        "                           <option value='c'>Član udruženja</option>" +
+        "                           <option value='u'>Član upravnog odbora</option>" +
+        "                       </select></label>" +
+        "               </div>" +
+        "               <button type='submit' name='noviClan' id='noviClan' class='expanded button'>Kreiraj novog člana</button>" +
+        "               <a href='#0' id='odustani' name='odustani' class='expanded button'>Odustani</a>" +
+        "           </form>" +
+        "       </div>" +
+        "</div>";
+
+    // dodaje se formular u DOM
+    $("#main-row").after(formular);
+    var $forma = $("#formular-noviClan");
+    $forma.css({
+        "display":"none",
+        "position":"absolute",
+        "top":"40px",
+        "z-index":"2",
+        "background-color":"white",
+        "border":"10px solid #e6e6e6",
+        "border-radius":"5px",
+        "width":"100%"
+    });
+    $forma.fadeIn("fast");
+
+    // Dugme za odustajanje
+    $("#odustani").on("click", function () {
+        $forma.fadeOut("fast", function () {
+            $forma.remove();
+        });
+    });
+
+    // Dugme za pohranjivanje podataka za novog clana
+    $("#noviClan").on("submit", function () {
+
+    })
+});
--- templates/home.php
+++ templates/home.php
@@ -4,16 +4,16 @@
 
 {% block body %}
 
-<div class="row">
+<div class="row" id="main-row">
     <div class="medium-3 columns show-for-medium">
-        <img id="korisnik_slika" src=" {{ session['Slika'] }}">
+        <img id="korisnik_slika" src=" {{ session['Slika'] }} ">
         <table>
             <tr><td id="ime"> {{ session['Ime'] ~ ' ' ~ session['Prezime'] }} </td></tr>
             <tr><td id="telefon"> {{ session['Telefon'] }}  </td></tr>
             <tr><td id="email"> {{ session['Email'] }}  </td></tr>
         </table>
         <form action="../sql/odjavljivanje.php" method="post">
-            <button type="submit" class="expanded button">Izloguj se</button>
+            <button type="submit" class="expanded button">Odjavi se</button>
         </form>
 
     </div>
@@ -21,6 +21,9 @@
 
         <ul class="tabs" data-tabs id="home-tabs">
             <li class="tabs-title is-active"><a href="#tab-obaveze">Obaveze</a></li>
+            {% if session['Tip'] == 'u' %}
+            <li class="tabs-title"><a href="#tab-administracija">Administracija</a></li>
+            {% endif %}
         </ul>
         <div class="tabs-content" data-tabs-content="home-tabs">
             <div class="tabs-panel is-active" id="tab-obaveze">
@@ -51,7 +54,18 @@
                 <a class="expanded button" href="#0">Učitaj još</a>
             </div>
         </div>
+        {% if session['Tip'] == 'u' %}
+        <div class="tabs-content" data-tabs-content="home-tabs">
+            <div class="tabs-panel" id="tab-administracija">
+                <label>Članovi
+                    <br>
+                    <button type="button" class="button" name="kreirajClana" id="kreirajClana">Kreiraj novog člana</button>
+                </label>
 
+            </div>
+        </div>
+
+        {% endif %}
     </div>
 </div>
 {% endblock %}
