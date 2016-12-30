f369db9c25e3a726ddc1e1c5a062b3ca8849b5c7
djinx <key13043@gmail.com>
2016-12-30 12:38:39 +0100

    Adding new task to selected user

--- sql/nova_obaveza.php
+++ sql/nova_obaveza.php
@@ -15,17 +15,31 @@ $opis = $_POST['OpisObaveze'];
 $datum = $_POST['DatumPocetkaObaveze'];
 $deadline = $_POST['Deadline'];
 $tim = $_POST['Tim'];
+$korisnik = $_POST['Korisnik'];
 $datum1 =  date('Y-m-d', strtotime($datum));
 $deadline1 =  date('Y-m-d', strtotime($deadline));
 
-
-$sql = 'insert into obaveza(`idObaveze`, `Naziv`, `Opis`, `Datum_pocetka`, `Deadline`, `Odradjena`, `idTima`) VALUES(NULL, ?, ?, ?, ?, 0, ?)';
+//pravljenje nove obaveze
+$sql = 'INSERT INTO obaveza(`idObaveze`, `Naziv`, `Opis`, `Datum_pocetka`, `Deadline`, `Odradjena`, `idTima`) VALUES(NULL, ?, ?, ?, ?, 0, ?)';
 $preparedQuery = $db->prepare($sql);
 $preparedQuery->bind_param("ssssi", $naziv, $opis, $datum1, $deadline1, $tim);
+$preparedQuery->execute();
+
+//echo $naziv." ".$opis." ".$datum1." ".$deadline1." ".$tim;
+
+//izdvajanje obaveze sa najvecim idObaveze - to je obaveza koja je poslednja uneta
+$query = 'SELECT max(idObaveze) FROM obaveza';
+$result=mysqli_query($db, $query) or die("Problem prilikom izvrsavanja upita");
+$row=mysqli_fetch_array($result);
+
+//dodavanje obaveze odabranom korisniku
+$sql = 'INSERT INTO `ima obavezu`(`idKorisnika`, `idObaveze`) VALUES (?, ?)';
+$preparedQuery = $db->prepare($sql);
+$preparedQuery->bind_param("ii", $korisnik, $row[0]);
 
 $preparedQuery->execute();
 
-echo $naziv." ".$opis." ".$datum1." ".$deadline1." ".$tim;
+echo $korisnik." ".$row[0];
 
 header("Location: ../public/");
 ?>
