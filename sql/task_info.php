d45832e0fea0eff3c43eefb08ca5ce44398dbb70
djinx <key13043@gmail.com>
2016-12-31 12:27:31 +0100

    Added functionality to the button Zavrsi obavezu

--- sql/task_info.php
+++ sql/task_info.php
@@ -15,7 +15,7 @@ if(isset($_SESSION['username'])){
 
     $idKorisnika = intval($_SESSION['username']['idKorisnika']);
     $num = $_GET['num'];
-    $query = "SELECT o.Naziv, o.Opis, o.Datum_pocetka, o.Deadline, t.Naziv FROM `ima obavezu` io JOIN obaveza o ON io.idObaveze = o.idObaveze JOIN tim t ON t.idTima = o.idTima WHERE o.Odradjena = 0 AND io.idKorisnika = ? ORDER BY o.Deadline";
+    $query = "SELECT o.idObaveze, o.Naziv, o.Opis, o.Datum_pocetka, o.Deadline, t.Naziv FROM `ima obavezu` io JOIN obaveza o ON io.idObaveze = o.idObaveze JOIN tim t ON t.idTima = o.idTima WHERE o.Odradjena = 0 AND io.idKorisnika = ? ORDER BY o.Deadline";
     if(intval($_GET['num']) != -1){
         $query = $query." LIMIT ?";
     }
@@ -27,7 +27,7 @@ if(isset($_SESSION['username'])){
         }
 
         if($preparedQuery->execute()){
-            $preparedQuery->bind_result($naziv, $opis, $datumPocetka, $deadline, $tim);
+            $preparedQuery->bind_result($idObaveze, $naziv, $opis, $datumPocetka, $deadline, $tim);
             while($preparedQuery->fetch()){
                 $type = "";
                 $date1 = new DateTime($deadline);
@@ -43,7 +43,7 @@ if(isset($_SESSION['username'])){
                     <p>Deadline: <?php echo $deadline; ?></p>
                     <div class="button-group">
                         <a href="#0" class="button">Pročitaj detalje</a>
-                        <button type="button" class="button">Završi obavezu</button>
+                        <button type="button" class="button" onclick="zavrsi_obavezu(<?php echo $idObaveze ?>)" >Završi obavezu</button>
                     </div>
                 </div>
                 <?php
