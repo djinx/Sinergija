b997166963c2ad37b434879ab8ef8ac21939bb9a
djinx <key13043@gmail.com>
2017-01-03 13:35:09 +0100

    Added functionality to the button Dodaj prijatelja

--- sql/add_friend.php
+++ sql/add_friend.php
@@ -6,4 +6,39 @@
  * Time: 3:19 PM
  */
 
+
+require_once('Database.php');
+
+$db = Database::getInstance();
+
+$errors = array();
+
+$projekat = $_POST['ProjekatIdP'];
+$prijatelj = $_POST['Prijatelj'];
+$zaduzen = $_POST['Zaduzen'];
+$status = $_POST['Status'];
+
+//provera napomene
+if(!empty($_POST['Napomena'])){
+    $napomena = $db->real_escape_string(trim($_POST['Napomena']));
+}else{
+    $napomena = NULL;
+}
+
+//upis podataka iz forme u bazu
+if($napomena){
+    $query =
+        " INSERT INTO `zaduzen`(`idKorisnika`, `idProjekta`, `idSponzora`, `Status`, `Napomena`) 
+          VALUES (?, ?, ?, ?, ?)";
+    $preparedQuery = $db->prepare($query);
+    $preparedQuery->bind_param("sssss", $zaduzen, $projekat, $prijatelj, $status, $napomena);
+} else{
+    $query =
+        " INSERT INTO `zaduzen`(`idKorisnika`, `idProjekta`, `idSponzora`, `Status`, `Napomena`) 
+          VALUES (?, ?, ?, ?, NULL)";
+    $preparedQuery = $db->prepare($query);
+    $preparedQuery->bind_param("ssss", $zaduzen, $projekat, $prijatelj, $status);
+}
+$preparedQuery->execute();
+
 header("Location: ../public/");
