4dba508b56ac9ceb92405b83a16af4e4a7c2e470
djinx <key13043@gmail.com>
2017-01-05 13:44:14 +0100

    Automatically insert user into Ucestvuje when creating new project

--- sql/create_project.php
+++ sql/create_project.php
@@ -6,11 +6,15 @@
  * Time: 1:49 PM
  */
 
+session_start();
+
 require_once('Database.php');
 
 $db = Database::getInstance();
 
 $errors = array();
+
+$idKorisnika = $_SESSION['username']['idKorisnika'];
 
 //provera naziva
 if(!empty($_POST['NazivProjekta'])){
@@ -32,13 +36,49 @@ $datumPD = $_POST['DatumPocetkaDogadjaja'];
 $pd = date('Y-m-d', strtotime($datumPD));
 $datumKD = $_POST['DatumKrajaDogadjaja'];
 $kd = date('Y-m-d', strtotime($datumKD));
+$idTima = $_POST['Tim'];
 
 
+$db->autocommit(false);
+//unos novog projekta
 $query =
     " INSERT INTO `projekat`(`idProjekta`, `naziv`, `opis`, `Pocetak_rada`, `Kraj_rada`, `Pocetak_dogadjaja`, `Kraj_dogadjaja`)
       VALUES (NULL, ?, ?, ?, NULL , ?, ?)";
 $preparedQuery = $db->prepare($query);
 $preparedQuery->bind_param("sssss", $naziv, $opis, $pr, $pd, $kd);
-$preparedQuery->execute();
+if($preparedQuery->execute()){
+    $preparedQuery->close();
+
+    //izdvajanje identifikatora novog projekta
+    $query = "SELECT max(`idProjekta`) FROM `Projekat`";
+    $preparedQuery = $db->prepare($query);
+    if($preparedQuery->execute()) {
+        $preparedQuery->bind_result($idProjekta);
+        $preparedQuery->fetch();
+        $preparedQuery->close();
+
+        //unos korisnika koji je napravio projekat u tabelu ucestvuje
+        $query =
+            " INSERT INTO `ucestvuje` (`idKorisnika`, `idProjekta`, `idTima`)
+              VALUES (?, ?, ?)";
+        $preparedQuery = $db->prepare($query);
+        $preparedQuery->bind_param("iii", $idKorisnika, $idProjekta, $idTima);
+        if($preparedQuery->execute()){
+            $db->commit();
+            // echo "Projekat je uspesno napravljen!";
+            header("Location: ../public/");
+            return;
+        }else{
+            $db->rollback();
+            die("Postoji problem unosom informacija o ucesniku!");
+        }
+    } else{
+        $db->rollback();
+        die("Postoji problem sa dohvatanjem informacija o novom projektu");
+    }
+} else{
+    $db->rollback();
+    die("Postoji problem sa unosom informacija o novom projektu");
+}
 
 header("Location: ../public/");
