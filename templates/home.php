8021dcbe86ea7ffef8792e3f423c409238691616
djinx <key13043@gmail.com>
2017-01-18 15:58:18 +0100

    Added scroll bar to inbox and sent

--- templates/home.php
+++ templates/home.php
@@ -49,8 +49,7 @@
                 <div class="row">
                     <div class="small-12 medium-4 large-4 columns" id="dugmici">
                         <button type="button" class="button" name="osveziSadrzaj" id="osveziSadrzaj">Osveži sadržaj</button>
-                        <br/>
-                        <button type="button" class="button" name="posaljiNovu" id="posaljiNovu">Pošalji novu poruku</button>
+                        <button type="button" class="button" name="posaljiNovu" id="posaljiNovu">Pošalji poruku</button>
 
                         <ul class="accordion" id="primljeneAcc" data-accordion="primljeneAcc" data-allow-all-closed="true" data-multi-expand="true">
                             <li class="accordion-navigation" data-accordion-item="" role="presentation">
